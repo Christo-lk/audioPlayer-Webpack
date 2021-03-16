e9aab751e8c61592b90677a8f06a7dc13d7b6840
Christo-lk <73858996+Christo-lk@users.noreply.github.com>
2021-03-16 19:59:15 +1300

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.join(__dirname, '../server/public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        include: path.join(__dirname, 'style.css'),
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../server/public')
+  }
+}
